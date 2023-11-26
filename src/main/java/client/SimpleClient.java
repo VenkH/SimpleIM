beb538c3a27110fe46b739624c6a58518c274363
Venk <3494857305@qq.com>
2023-11-26 19:40:44 +0800

    feat：搭建起简单的客户端和服务端

---
+++ src/main/java/client/SimpleClient.java
@@ -0,0 +1,89 @@
+package client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class SimpleClient {
+    private static final int MAX_RETRY = 5;
+    public static void main(String[] args) {
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap
+                // 1.指定线程模型
+                .group(workerGroup)
+                // 2.指定 IO 类型为 NIO
+                .channel(NioSocketChannel.class)
+                // 3.IO 处理逻辑
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    public void initChannel(SocketChannel ch) {
+                        ch.pipeline().addLast(new FirstClientHandler());
+                    }
+                });
+        // 4.建立连接
+        connect(bootstrap, "localhost", 9000, MAX_RETRY);
+    }
+
+    /**
+     * 建立连接，连接失败再次尝试
+     * @param bootstrap 启动引导类
+     * @param host 主机号
+     * @param port 端口号
+     */
+    private static void connect(Bootstrap bootstrap, String host, int port) {
+        bootstrap.connect(host, port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("连接成功!");
+            } else {
+                System.err.println("连接失败，开始重连");
+                connect(bootstrap, host, port);
+            }
+        });
+    }
+
+
+    /**
+     * 建立连接，连接失败再次尝试，限制重连次数
+     * 重连间隔时间按照2的幂次方增长
+     * @param bootstrap 启动引导类
+     * @param host 主机号
+     * @param port 端口号
+     * @param retry 重连次数
+     */
+    private static void connect(Bootstrap bootstrap, String host, int port, int retry) {
+
+        bootstrap.connect(host, port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("连接成功!");
+            } else if (retry == 0) {
+                System.err.println("重试次数已用完，放弃连接！");
+            } else {
+                // 第几次重连
+                int order = (MAX_RETRY - retry) + 1;
+                // 本次重连的间隔
+                int delay = 1 << order;
+                System.err.println(new Date() + ": 连接失败，第" + order + "次重连……");
+
+                bootstrap
+                        // 拿到bootstrap配置参数的抽象
+                         .config()
+                        // 拿到EventLoopGroup，也就是WorkerGroup
+                         .group()
+                        // 调用定时任务
+                         .schedule(
+                                () -> connect(bootstrap, host, port, retry - 1)
+                                , delay
+                                , TimeUnit.SECONDS);
+            }
+        });
+    }
+
+
+}
---
+++ src/main/java/server/SimpleServer.java
@@ -0,0 +1,57 @@
+package server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+
+public class SimpleServer {
+    public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap
+                .group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                // 开启心跳机制
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                // 关闭Nagle算法，提高消息实时性
+                .childOption(ChannelOption.TCP_NODELAY, true)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) {
+                        ch.pipeline().addLast(new FirstServerHandler());
+                    }
+                });
+
+        bind(serverBootstrap, 9000);
+
+    }
+
+    /**
+     * 绑定端口，当端口被占用时，端口递增继续尝试
+     * @param serverBootstrap 引导类
+     * @param port 端口号
+     */
+    private static void bind(final ServerBootstrap serverBootstrap, final int port) {
+        serverBootstrap.bind(port).addListener(new GenericFutureListener<Future<? super Void>>() {
+            @Override
+            public void operationComplete(Future<? super Void> future) {
+                if (future.isSuccess()) {
+                    System.out.println("端口[" + port + "]绑定成功!");
+                } else {
+                    System.err.println("端口[" + port + "]绑定失败!");
+                    bind(serverBootstrap, port + 1);
+                }
+            }
+        });
+    }
+
+
+}
